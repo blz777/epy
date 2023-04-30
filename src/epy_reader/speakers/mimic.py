42afeaa17743cb06a169f79e299cf0913527abef
Anton Petrov <blz777@gmail.com>
2023-04-30 12:53:21 +0300

    mimic3: Works after pressing j but cannot move in book and cannot stop.

--- src/epy_reader/speakers/mimic.py
+++ src/epy_reader/speakers/mimic.py
@@ -1,31 +1,49 @@
 import shutil
 import subprocess
+import threading
 
 from epy_reader.speakers.base import SpeakerBaseModel
 
 
 class SpeakerMimic(SpeakerBaseModel):
-    cmd = "mimic"
-    available = bool(shutil.which("mimic"))
+    cmd = "mimic3"
+    available = bool(shutil.which("mimic3"))
 
-    def speak(self, text: str) -> None:
-        self.process = subprocess.Popen(
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.audio_done = threading.Event()
+
+    def speak(self, text: str):
+        self.audio_done.clear()
+
+        self.mimic3_process = subprocess.Popen(
             [self.cmd, *self.args],
             text=True,
             stdin=subprocess.PIPE,
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.STDOUT,
+            stdout=subprocess.PIPE,
         )
-        assert self.process.stdin
-        self.process.stdin.write(text)
-        self.process.stdin.close()
+
+        self.process = subprocess.Popen(
+            ["play", "-t", "wav", "-"],
+            stdin=self.mimic3_process.stdout,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+
+        self.mimic3_process.stdin.write(text)
+        self.mimic3_process.stdin.close()
+
+        self.wait_for_audio()
+
+    def wait_for_audio(self):
+        self.process.wait()
+        self.audio_done.set()
 
     def is_done(self) -> bool:
-        return self.process.poll() is not None
+        return self.audio_done.is_set()
 
     def stop(self) -> None:
         self.process.terminate()
-        # self.process.kill()
 
     def cleanup(self) -> None:
         pass
